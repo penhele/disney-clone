1eb0f2cd98f849084dc30ee7f78ab9b45af51050
penhele <stephenhelk@gmail.com>
2025-08-10 11:00:49 +0700

    feat: add header n responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </>
   );
 }
