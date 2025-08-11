9c67bc62eb892db5387654cbf5c2ce9d9e2678fe
penhele <stephenhelk@gmail.com>
2025-08-11 22:27:11 +0700

    feat: movie list

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -33,11 +33,12 @@ function Slider() {
         className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer "
         onClick={() => sliderLeft(elementRef.current)}
       />
+
       <HiChevronRight
         className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
         onClick={() => sliderRight(elementRef.current)}
       />
-
+      
       <div
         className="flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth"
         ref={elementRef}
@@ -45,7 +46,7 @@ function Slider() {
         {movieList.map((item) => (
           <img
             src={IMAGE_BASE_URL + item.backdrop_path}
-            className="min-w-full  md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out"
+            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out"
           />
         ))}
       </div>
